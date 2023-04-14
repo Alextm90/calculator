1cfd56d1c9d242f6b10060659fddf517050bed04
Alex Miserandino <amiserandino20@gmail.com>
2023-04-14 13:05:15 -0700

    worked on design

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -62,25 +62,25 @@ function App() {
     <div className="App">
       <div id='calculator'>
        <div id="container">
-       <div id='display-history'>{history}{equal}</div>
-       <div id="display">{display}</div>
-       <button id="clear" onClick={clearDisplay}>AC</button>
-       <button id="equals" onClick={evaluateStr}>=</button>
-       <button id="seven" value={7} onClick={(e) => setInput(e)}>7</button>
-       <button id="eight" value={8} onClick={(e) => setInput(e)}>8</button>
-       <button id="nine" value={9} onClick={(e) => setInput(e)}>9</button>
-       <button id="four" value={4} onClick={(e) => setInput(e)}>4</button>
-       <button id="five" value={5} onClick={(e) => setInput(e)}>5</button>
-       <button id="six" value={6} onClick={(e) => setInput(e)}>6</button>
-       <button id="one" value={1} onClick={(e) => setInput(e)}>1</button>
-       <button id="two" value={2} onClick={(e) => setInput(e)}>2</button>
-       <button id="three" value={3} onClick={(e) => setInput(e)}>3</button>
-       <button id="zero" value={0} onClick={(e) => setInput(e)}>0</button>
-       <button id="add" value={" + "} onClick={(e) => setInput(e)}>+</button>
-       <button id="subtract" value={" - "} onClick={(e) => setInput(e)}>-</button>
-       <button id="multiply" value={" * "} onClick={(e) => setInput(e)}>X</button>
-       <button id="divide" value={" / "} onClick={(e) => setInput(e)}>/</button>
-       <button id="decimal" value={"."} onClick={(e) => setInput(e)}>.</button>
+        <div id='display-history'>{history}{equal}</div>
+        <div id="display">{display}</div>
+        <button id="clear" onClick={clearDisplay}>AC</button>
+        <button id="equals" onClick={evaluateStr}>=</button>
+        <button id="seven" value={7} onClick={(e) => setInput(e)}>7</button>
+        <button id="eight" value={8} onClick={(e) => setInput(e)}>8</button>
+        <button id="nine" value={9} onClick={(e) => setInput(e)}>9</button>
+        <button id="four" value={4} onClick={(e) => setInput(e)}>4</button>
+        <button id="five" value={5} onClick={(e) => setInput(e)}>5</button>
+        <button id="six" value={6} onClick={(e) => setInput(e)}>6</button>
+        <button id="one" value={1} onClick={(e) => setInput(e)}>1</button>
+        <button id="two" value={2} onClick={(e) => setInput(e)}>2</button>
+        <button id="three" value={3} onClick={(e) => setInput(e)}>3</button>
+        <button id="zero" value={0} onClick={(e) => setInput(e)}>0</button>
+        <button id="add" value={" + "} onClick={(e) => setInput(e)}>+</button>
+        <button id="subtract" value={" - "} onClick={(e) => setInput(e)}>-</button>
+        <button id="multiply" value={" * "} onClick={(e) => setInput(e)}>X</button>
+        <button id="divide" value={" / "} onClick={(e) => setInput(e)}>/</button>
+        <button id="decimal" value={"."} onClick={(e) => setInput(e)}>.</button>
        </div>
      </div>
     </div>
